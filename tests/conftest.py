4dd9fe6a2483b3c1741fdb58ad2bd0a2ccc18683
Zach Gallagher <zachagallagher@gmail.com>
2020-04-11 07:31:02 -0700

    complete ch2;

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
